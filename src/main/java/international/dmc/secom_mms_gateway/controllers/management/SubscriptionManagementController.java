9703f14f9206bcb54cd5da230d1099322f327d3e
Oliver Steensen-Bech Haagh <oliver@dmc.international>
2024-10-24 10:27:35 +0200

    Remember to annotate the request body

--- src/main/java/international/dmc/secom_mms_gateway/controllers/management/SubscriptionManagementController.java
+++ src/main/java/international/dmc/secom_mms_gateway/controllers/management/SubscriptionManagementController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,7 +36,7 @@ public class SubscriptionManagementController {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public ResponseEntity<Subscription> createNewSubscription(Subscription subscription) {
+    public ResponseEntity<Subscription> createNewSubscription(@RequestBody Subscription subscription) {
         Subscription newSubscription;
         try {
             newSubscription = subscriptionService.addSubscription(subscription);
